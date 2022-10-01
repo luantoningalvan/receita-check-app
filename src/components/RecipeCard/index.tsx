980a6c657704a0489f447bdbb0e042f175e65b08
Luan Tonin Galvan <ltoningalvan@gmail.com>
2022-10-01 19:11:36 -0300

    feat: mostra versão da imagem com blur durante carregamento

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { Container, Cover, Title } from "./styles";
+import { Container, CoverContainer, Cover, Title, BlurCover } from "./styles";
 import { Recipe } from "../../common/interfaces/Recipe";
+import { Animated } from "react-native";
+import { Blurhash } from "react-native-blurhash";
 
 interface RecipeCardProps {
   recipe: Recipe;
@@ -9,14 +11,32 @@ interface RecipeCardProps {
 
 export function RecipeCard({ recipe }: RecipeCardProps) {
   const navigation = useNavigation<any>();
+  const fadeAnim = React.useRef(new Animated.Value(1)).current;
+
+  function onLoad() {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }
 
   return (
     <Container onPress={() => navigation.navigate("ViewRecipe", { recipe })}>
-      <Cover
-        source={{
-          uri: recipe.imageUrl,
-        }}
-      />
+      <CoverContainer>
+        <BlurCover style={{ opacity: fadeAnim }}>
+          <Blurhash
+            blurhash="LGFFaXYk^6#M@-5c,1J5@[or[Q6."
+            style={{ flex: 1 }}
+          />
+        </BlurCover>
+        <Cover
+          source={{
+            uri: recipe.imageUrl,
+          }}
+          onLoad={onLoad}
+        />
+      </CoverContainer>
       <Title>{recipe.title}</Title>
     </Container>
   );
--- src/components/RecipeCard/styles.ts
+++ src/components/RecipeCard/styles.ts
@@ -1,15 +1,33 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { Animated } from "react-native";
 
 export const Container = styled.TouchableOpacity`
   margin: 12px 0px;
   width: 47%;
 `;
 
+export const CoverContainer = styled.View`
+  width: 100%;
+  height: ${RFValue(140)}px;
+  border-radius: 8px;
+  overflow: hidden;
+`;
+
 export const Cover = styled.Image`
   width: 100%;
   height: ${RFValue(140)}px;
   border-radius: 8px;
+`;
+
+export const BlurCover = styled(Animated.View)`
+  border-radius: 8px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
 `;
 
 export const Title = styled.Text`
