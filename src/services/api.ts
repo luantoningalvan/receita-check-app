3aec289fd8199356402053e6861062948f8f85fa
Luan Tonin Galvan <ltoningalvan@gmail.com>
2022-11-02 14:39:54 -0300

    refactor: remove token desnecessário da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,4 @@ const api = axios.create({
   baseURL: "https://api.receitacheck.com.br",
 });
 
-api.defaults.headers.common["Authorization"] =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoicmVjZWl0YS1jaGVjay1hcHAifQ.P0AIO3tFnFJvuGHEqR-hrcYEQsHXMPVcV57GUqwocmg";
-
 export { api };
